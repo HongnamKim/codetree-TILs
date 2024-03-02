e4650101f02fb33763adb8442fa1a661f4ad6523
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-02 15:44:47 +0900

    Title: 진법 변환, Time: 207ms, Memory: 10MB, Status: Passed - CodeTree

--- 240302/진법 변환/base-conversion.java
+++ 240302/진법 변환/base-conversion.java
@@ -8,13 +8,6 @@ public class Main {
         String input = sc.next();
         int order = sc.nextInt();
 
-        long result = 0L;
-
-        for(int i = 0; i < input.length(); i++){
-            int convert = Character.getNumericValue(input.charAt(i)) * (int) Math.pow(order, input.length() - i - 1);
-            result += convert;
-        }
-
-        System.out.println(result);
+        System.out.println(Integer.parseInt(input, order));
     }
 }
