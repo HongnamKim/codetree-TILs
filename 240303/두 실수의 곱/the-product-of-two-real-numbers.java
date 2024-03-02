7abb4f09af2e2baf9a5921a39f01bcca4f440d36
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-03 07:08:20 +0900

    Title: 두 실수의 곱, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240303/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        float a = 5.26F;
+        float b = 8.27F;
+        float c = a * b;
+        System.out.printf("%.03f", c);
+    }
+}
