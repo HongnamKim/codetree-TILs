56c150b8a66e7a0e19c6e2ecdbe8568c5db5ff62
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-09 09:22:06 +0900

    Title: Factorial, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/Factorial/factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+
+    }
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        return factorial(n-1) * n;
+
+    }
+}
