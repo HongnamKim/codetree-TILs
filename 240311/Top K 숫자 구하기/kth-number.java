22a88c3274f73c28ab61727f16d531a05d1b3800
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-11 22:40:11 +0900

    Title: Top K 숫자 구하기, Time: 197ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240311/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(nums[k-1]);
+    }
+}
