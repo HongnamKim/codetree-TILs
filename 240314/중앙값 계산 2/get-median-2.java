85556cdad0e092545f15c9e7c66d76a1ca54cb4b
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-14 22:52:55 +0900

    Title: 중앙값 계산 2, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+            if(i % 2 == 0){
+                Arrays.sort(nums, 0, i + 1);
+                System.out.print(nums[i/2] + " ");
+            }
+        }
+    }
+}
