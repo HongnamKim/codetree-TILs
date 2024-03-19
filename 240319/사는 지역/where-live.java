b0dbd8bd84d079b2aa9df8f0c759ce0833a46be9
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-03-19 22:22:54 +0900

    Title: 사는 지역, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/사는 지역/where-live.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Address[] addresses = new Address[n];
+
+        for(int i = 0; i < n; i++){
+            addresses[i] = new Address(sc.next(), sc.next(), sc.next());
+        }
+        String a = "lee";
+        String b = "park";
+
+        //System.out.println(b.compareTo(a));
+
+        int latestIndex = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(addresses[i].name.compareTo(addresses[i + 1].name) < 0) {
+                latestIndex = i + 1;
+            }
+        }
+
+        System.out.println(addresses[latestIndex].toString());
+    }
+
+    public static class Address {
+        public String name;
+        public String code;
+        public String city;
+
+        public Address(String name, String code, String city) {
+            this.name = name;
+            this.code = code;
+            this.city = city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        @Override
+        public String toString() {
+            return "name " + this.name + "\n" +
+                    "addr " + this.code + "\n" +
+                    "city " + this.city + "\n";
+        }
+    }
+}
