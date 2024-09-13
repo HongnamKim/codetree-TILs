d9af4e860f0245b4c9873d3a0bb45a22414af047
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-09-14 01:42:51 +0900

    Title: 음수 구별, Time: 205ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240914/음수 구별/distinguish-negative-number.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [a] = input[0].split(' ').map(Number);
+
+if(a >= 0) {
+    console.log(a);
+} else {
+    console.log('minus');
+}
