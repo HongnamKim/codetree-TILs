3d60c74fb4c976ca5012d5a00738d766745a2404
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-09-18 00:05:39 +0900

    Title: 최대로 겹치는 구간, Time: 159ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240918/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = input[0].split(' ').map(Number);
+
+const offset = 0;
+
+const board = new Array(10).fill(0);
+
+for(let i = 1; i <= n; i++){
+    let [start, end] = input[i].split(' ').map(Number);
+    start = start + offset;
+    end = end + offset;
+
+    //console.log(`${start} ${end}`);
+
+    for(let i = start; i < end; i++) {
+        board[i]++;
+    }
+
+    
+}
+
+const result = Math.max(...board);
+
+console.log(result);
