21403b2a350148f1eb199b05503c8d79af542595
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-09-23 00:18:59 +0900

    Title: 흰검 칠하기, Time: 196ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240923/흰검 칠하기/painting-white-black.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = +input[0];
+
+//console.log(`n: ${n}`);
+
+const OFFSET = 5;
+const R = 2 * OFFSET + 1;
+let currentLocation = OFFSET;
+
+const board = Array(R).fill('');
+
+for(let i = 1; i <= n; i++) {
+    let [x, direction] = input[i].split(' ');
+
+    x = +x;
+    
+    if(direction === 'R'){
+        for(let i = 0; i < x; i++) {
+            board[currentLocation] += 'b'
+            if(i !== x - 1){
+                currentLocation++;
+            }
+            
+        }
+    } else if(direction === 'L'){
+        for(let i = 0; i < x; i++) {
+            board[currentLocation] += 'w'
+            if(i !== x - 1){
+                currentLocation--;
+            }
+            
+        }
+    }
+
+}
+
+//console.log(board);
+
+let W = 0, B = 0, G = 0;
+
+for(let i = 0; i < R; i++){
+    const colors = board[i].split('');
+
+    if(colors.length === 0) {
+        continue;
+    }
+
+    if(colors.length > 3) {
+        G++;
+    }else if(colors[colors.length - 1] === 'w') {
+        W++;
+    }else{
+        B++;
+    }
+}
+
+console.log(`${W} ${B} ${G}`)
