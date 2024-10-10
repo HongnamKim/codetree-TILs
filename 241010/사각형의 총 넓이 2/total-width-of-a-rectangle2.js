36ba9b08818ff1b1fda9849ce51907d0a5122e4b
Hongnam Kim <111332312+HongnamKim@users.noreply.github.com>
2024-10-10 22:06:52 +0900

    Title: 사각형의 총 넓이 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241010/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = +input[0];
+
+const OFFSET = 6;
+const R = 2 * OFFSET + 1;
+
+const board = [];
+
+for(let i = 0; i < R; i++){
+    board.push(Array(R).fill(0));
+}
+
+//console.log(board);
+
+let area = 0;
+
+for(let i = 1; i <= n; i++){
+    const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
+
+    for(let j = x1 + OFFSET; j < x2 + OFFSET; j++) {
+        for(let k = y1 + OFFSET; k < y2 + OFFSET; k++) {
+            if(board[j][k] != 1) {
+                area++;
+                board[j][k] = 1
+            }
+        }
+    }
+}
+
+//console.log(board);
+console.log(area);
